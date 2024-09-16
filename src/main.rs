6c7e32654b31b718a2b02cd77844b0c589e6db5e
itsgerliz <gerworks.hs@gmail.com>
2024-09-16 22:09:42 +0200

    feat: hexreader now checks if a file name has been specified

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = std::env::args().collect::<Vec<String>>();
+	let argc: usize = argv.len();
+
+	if argc != 2 {
+		println!("Usage: {} <file name>", argv[0]);
+		std::process::exit(1);
+	}
 }
