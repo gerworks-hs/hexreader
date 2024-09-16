7eaaa736fc9fff2c77252dece60c9b95c27fbd73
itsgerliz <gerworks.hs@gmail.com>
2024-09-16 22:48:56 +0200

    feat: hexreader is now able to check if the file path is valid or not

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,14 @@ fn main() {
 		println!("Usage: {} <file name>", argv[0]);
 		std::process::exit(1);
 	}
+
+	let file_path: &std::path::Path = std::path::Path::new(&argv[1]);
+
+	let file_data: std::fs::File = match std::fs::File::open(file_path) {
+		Ok(file_struct) => {file_struct}
+		Err(_) => {
+			println!("Error trying to access file path ({})", argv[1]);
+			std::process::exit(1);
+		}
+	};
 }
